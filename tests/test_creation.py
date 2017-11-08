00afdaa489a35b13bbc1ba982cfc4329bb7713db
apra93 <dd.apra93@gmail.com>
2017-11-07 17:47:58 -0800

    TST: Added better tests and fixed the json file.

--- tests/test_creation.py
+++ tests/test_creation.py
@@ -1,8 +1,14 @@
+############
+# Standard #
+############
 import os
 import shutil
 
-from cookiecutter import main
+###############
+# Third Party #
+###############
 import pytest
+from cookiecutter import main
 
 CCDS_ROOT = os.path.abspath(
                 os.path.join(
@@ -11,7 +17,6 @@ CCDS_ROOT = os.path.abspath(
                     os.pardir
                 )
             )
-
 
 @pytest.fixture(scope='function')
 def default_baked_project(tmpdir):
@@ -30,35 +35,8 @@ def default_baked_project(tmpdir):
     # cleanup after
     shutil.rmtree(out_dir)
 
-
-def test_readme(default_baked_project):
-    readme_path = os.path.join(default_baked_project, 'README.md')
-
-    assert os.path.exists(readme_path)
-    assert no_curlies(readme_path)
-
-
-def test_license(default_baked_project):
-    license_path = os.path.join(default_baked_project, 'LICENSE')
-
-    assert os.path.exists(license_path)
-    assert no_curlies(license_path)
-
-
-def test_requirements(default_baked_project):
-    reqs_path = os.path.join(default_baked_project, 'requirements.txt')
-
-    assert os.path.exists(reqs_path)
-    assert no_curlies(reqs_path)
-
-
-def test_makefile(default_baked_project):
-    makefile_path = os.path.join(default_baked_project, 'Makefile')
-
-    assert os.path.exists(makefile_path)
-    assert no_curlies(makefile_path)
-
-
+# Test the folders were created properly
+    
 def test_folders(default_baked_project):
     expected_dirs = [
         'data',
@@ -67,11 +45,12 @@ def test_folders(default_baked_project):
         os.path.join('data', 'processed'),
         os.path.join('data', 'raw'),
         'docs',
+        os.path.join('figures', 'finalized'),
+        os.path.join('figures', 'unsorted'),
+        'logs',
         'models',
         'notebooks',
         'references',
-        'reports',
-        os.path.join('reports', 'figures'),
         'src',
         os.path.join('src', 'data'),
         os.path.join('src', 'features'),
@@ -90,10 +69,10 @@ def test_folders(default_baked_project):
 
     assert len(set(abs_expected_dirs + ignored_dirs) - set(abs_dirs)) == 0
 
-
 def no_curlies(filepath):
-    """ Utility to make sure no curly braces appear in a file.
-        That is, was jinja able to render everthing?
+    """
+    Utility to make sure no curly braces appear in a file. That is, was jinja
+    able to render everthing?
     """
     with open(filepath, 'r') as f:
         data = f.read()
@@ -106,5 +85,40 @@ def no_curlies(filepath):
     ]
 
     template_strings_in_file = [s in data for s in template_strings]
-
     return not any(template_strings_in_file)
+
+# Test all the top level files were created propertly
+
+expected_files = [
+    '.coveragerc',
+    '.gitignore',
+    '.landscape.yml',
+    '.travis.yml',
+    'LICENSE',
+    'Makefile',
+    'MANIFEST.in',
+    'README.rst',
+    'requirements.txt',
+    'run_tests.py',
+    'setup.py',
+    'setup.cfg',
+    'versioneer.py',
+    os.path.join('src', '__init__.py'),
+    os.path.join('src', 'data', '__init__.py'),
+    os.path.join('src', 'data', 'make_dataset.py'),    
+    os.path.join('src', 'features', '__init__.py'),
+    os.path.join('src', 'features', 'build_features.py'),    
+    os.path.join('src', 'models', '__init__.py'),
+    os.path.join('src', 'models', 'predict_model.py'),
+    os.path.join('src', 'models', 'train_model.py'),    
+    os.path.join('src', 'visualization', '__init__.py'),
+    os.path.join('src', 'visualization', 'visualize.py'),    
+    ]
+
+@pytest.mark.parametrize("expected_file", expected_files)
+def test_file(default_baked_project, expected_file):
+    path_expected_file = os.path.join(default_baked_project, expected_file)
+    assert os.path.exists(path_expected_file)
+    assert no_curlies(path_expected_file)
+
+
