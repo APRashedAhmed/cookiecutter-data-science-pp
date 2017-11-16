70f138eae638f0b8c3603ee0b031e3b49332e1e2
apra93 <dd.apra93@gmail.com>
2017-11-15 22:52:27 -0800

    TST: Made creation tests more general by walking though the template directory and then checking the created directory.

--- tests/test_creation.py
+++ tests/test_creation.py
@@ -9,22 +9,25 @@ from pathlib import Path
 # Third Party #
 ###############
 import pytest
+import simplejson as sjson
 from cookiecutter import main
 
-CCDS_ROOT = os.path.abspath(
+CCDSA_ROOT = os.path.abspath(
                 os.path.join(
                     __file__,
                     os.pardir,
                     os.pardir
                 )
             )
+TEMPLATE_DIR = Path(CCDSA_ROOT) / "{{ cookiecutter.repo_name }}"
+CC_JSON = sjson.load(open(str(Path(CCDSA_ROOT) / "cookiecutter.json")))
 
 @pytest.fixture(scope='function')
 def default_baked_project(tmpdir):
     out_dir = str(tmpdir.mkdir('data-project'))
 
     main.cookiecutter(
-        CCDS_ROOT,
+        CCDSA_ROOT,
         no_input=True,
         extra_context={},
         output_dir=out_dir
@@ -36,71 +39,66 @@ def default_baked_project(tmpdir):
     # cleanup after
     shutil.rmtree(out_dir)
 
-# Test the folders were created properly
-expected_folders = [
-    'data',
-    os.path.join('data', 'external'),
-    os.path.join('data', 'interim'),
-    os.path.join('data', 'processed'),
-    os.path.join('data', 'raw'),
-    'docs',
-    os.path.join('figures', 'finalized'),
-    os.path.join('figures', 'unsorted'),
-    'logs',
-    'models',
-    'notebooks',
-    'references',
-    'src',
-    os.path.join('src', 'data'),
-    os.path.join('src', 'features'),
-    os.path.join('src', 'models'),
-    os.path.join('src', 'visualization')
-]
+def substitute_cookiecutter_json(name_tuple):
+    """
+    Substitutes the cookiecutter template names with their values from the json
+    file.
+    """
+    substituted = []
+    for part in name_tuple:
+        # Check if the cookiecutter braces are in the name
+         if "{{ " in part and " }}" in part:
+             # Remove them if they are along with 'cookiecutter.'
+             part_temp = part.replace("{{ ", "")
+             part_temp = part_temp.replace(" }}", "")
+             part_temp = part_temp.replace("{{", "")
+             part_temp = part_temp.replace("}}", "")
+             part_temp = part_temp.replace("cookiecutter.", "")
+             # Replace with the json value
+             if part_temp in CC_JSON.keys():
+                 substituted.append(CC_JSON[part_temp])
+                 continue
+         else:
+             substituted.append(part)
+    return substituted
 
-expected_files = [
-    '.coveragerc',
-    '.gitignore',
-    '.landscape.yml',
-    '.travis.yml',
-    'LICENSE',
-    'Makefile',
-    'MANIFEST.in',
-    'README.rst',
-    'requirements.txt',
-    'run_tests.py',
-    'setup.py',
-    'setup.cfg',
-    'versioneer.py',
-    os.path.join('docs', 'commands.rst'),
-    os.path.join('docs', 'conf.py'),
-    os.path.join('docs', 'getting-started.rst'),
-    os.path.join('docs', 'index.rst'),
-    os.path.join('docs', 'make.bat'),
-    os.path.join('docs', 'Makefile'),
-    os.path.join('docs', 'features.rst'),
-    os.path.join('docs', 'data.rst'),
-    os.path.join('docs', 'models.rst'),
-    os.path.join('docs', 'visualization.rst'),
-    os.path.join('docs', 'utils.rst'),
-    os.path.join('notebooks', '0.0-Notebook-Template.ipynb'),
-    os.path.join('src', '__init__.py'),
-    os.path.join('src', 'utils.py'),
-    os.path.join('src', 'data', '__init__.py'),
-    os.path.join('src', 'data', 'make_dataset.py'),    
-    os.path.join('src', 'features', '__init__.py'),
-    os.path.join('src', 'features', 'build_features.py'),    
-    os.path.join('src', 'features', 'preprocessing.py'),    
-    os.path.join('src', 'models', '__init__.py'),
-    os.path.join('src', 'models', 'predict_model.py'),
-    os.path.join('src', 'models', 'train_model.py'),    
-    os.path.join('src', 'visualization', '__init__.py'),
-    os.path.join('src', 'visualization', 'visualize.py'),    
-    ]
+def get_contents(root_dir=TEMPLATE_DIR, omitted_dirs=set([
+        ".git", ".cache", "__pycache__", ".ipynb_checkpoints"]),
+                 omitted_files=set([])):
+    """
+    Returns two lists of directories and files of the cookiecutter template
+    directory.
+    """
+    dirs, files = [], []
+    # Iterate through all contents of root_dir
+    for path in root_dir.iterdir():
+        # Only include the parts of the path that come after the template
+        # directory name
+        expected_path = Path("/".join(path.parts[path.parts.index(
+            TEMPLATE_DIR.name)+1:]))
+        # Make any substitutions that could be necessary
+        substituted_path = "/".join(substitute_cookiecutter_json(
+            expected_path.parts))
+        # Check if it is a directory that isnt supposed to be omitted
+        if path.is_dir() and path.name not in omitted_dirs:
+            dirs.append(substituted_path)
+            # Go into the directory and get the contents
+            dirs_recurse, files_recurse = get_contents(root_dir=path)
+            # Update with what we got
+            dirs += dirs_recurse
+            files += files_recurse
+        # If it is a file, check it isnt being omitted
+        elif path.is_file() and path.name not in omitted_files:
+            files.append(substituted_path)
+    return dirs, files
 
-@pytest.mark.parametrize("expected_folder", expected_folders)
-def test_folder(default_baked_project, expected_folder):
-    path_expected_folder = Path(default_baked_project) / expected_folder
-    assert path_expected_folder.exists()
+expected_directories, expected_files = get_contents()
+
+@pytest.mark.parametrize("path", expected_directories)
+def test_folder(default_baked_project, path):
+    path_expected_dir = Path(default_baked_project) / path
+    assert path_expected_dir.exists()
+    assert path_expected_dir.is_dir()
 
 def no_curlies(filepath):
     """
@@ -124,6 +122,7 @@ def no_curlies(filepath):
 def test_file(default_baked_project, expected_file):
     path_expected_file = Path(default_baked_project) / expected_file
     assert path_expected_file.exists()    
+    assert path_expected_file.is_file()
     assert no_curlies(str(path_expected_file))
 
 
