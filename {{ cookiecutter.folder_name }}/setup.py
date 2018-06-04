71408ed7a8eb760ef348232307322d385b195a88
apra <dd.apra93@gmail.com>
2018-06-04 16:13:53 -0400

    ENH: Updating travis.

--- {{ cookiecutter.folder_name }}/setup.py
+++ {{ cookiecutter.folder_name }}/setup.py
@@ -1,7 +1,7 @@
 import versioneer
 from setuptools import setup, find_packages
 
-setup(name='{{ cookiecutter.folder_name }}',
+setup(name='{{ cookiecutter.repo_name }}',
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
       license='{{ cookiecutter.open_source_license }}',
